05c3fbf076d9e8f1650d1a1e433fe327c446cacb
thecamboprince <dug21002@byui.edu>
2022-05-19 21:38:49 -0600

    $password change

--- library/connections.php
+++ library/connections.php
@@ -7,7 +7,7 @@ function phpmotorsConnect() {
     $server = 'localhost';
     $dbname = 'phpmotors';
     $username = 'iClient';
-    $password = 'mGrpAZ.z0/sa)F3@';
+    $password = 'evTn[vfLFQW(Pxhk';
     $dsn= "mysql:host=$server;dbname=$dbname";
     $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
